be6f8f537774c88c72ad7d153d4157ba0cb17c52
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-04-08 11:29:23 +0600

    Video 17 done!

--- sqltest/v-17-interview-questions.sql
+++ sqltest/v-17-interview-questions.sql
@@ -22,3 +22,31 @@ select lastname from Employee where Lastname like 'r%'
 -- q7 Write a query to show lastName  where lastName holds r, o or s
 select lastname from Employee where Lastname like '%R%o%s%'
 select lastname from Employee where Lastname like '%R%o%s' -- that means end with s
+-- q8 Write a query to show , WorkerId, Employee's FirstName where each employee salary is more than
+-- the average salary
+Select WorkerID,FirstName,Salary from Employee 
+where Salary > (Select AVG(Salary) from Employee)
+-- q9 Write a query to show the 5% increment of the salary as New_Salary
+Select WorkerID,FirstName,Salary,Salary*.05 as Increment,Salary+(Salary*.05) 
+as New_Salary from Employee
+-- q10 Write a query to show Employee FirstName, LastName , Salary of the Employee
+-- with lowest Salary
+-- by default when we use order by we can use order by clause for sorting ascending
+Select top 1 WorkerID,FirstName,LastName,Salary,Department from Employee 
+order by Salary
+-- q11 Write a query to show Employee FirstName, LastName , Salary of the Employee
+-- But with lowest salary for each department
+-- we can seperately execute inner qery to see the result
+-- Always remind that parition by is use with order by clause
+Select WorkerID,FirstName,LastName,Salary,Department from (
+Select WorkerID,FirstName,LastName,Salary,Department ,
+Row_Number() over (PARTITION by Department order by Salary) as SalaryRank
+from Employee 
+) As rankedEmployees
+where SalaryRank = 1
+
+-- q12 Write a query to show records in which department count of employees is
+-- greater than 1
+-- Select * from Employee group by Department having Count(Department) > 1 --common error
+Select Department,COUNT(Department) as TotalEmployee from Employee 
+group by Department having Count(Department) > 1
