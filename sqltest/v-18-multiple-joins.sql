df0c0dfc4ba83926950efc54d58ebcb76fa79b31
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-04-08 13:06:40 +0600

    Formatting the orderdate here!

--- sqltest/v-18-multiple-joins.sql
+++ sqltest/v-18-multiple-joins.sql
@@ -2,7 +2,8 @@ use school;
 select * from Orders
 -- query to find only the customer FirstName, E-mail , OrderDate and Amount
 -- for the last 200 days who buys more that 150 taka 
-select c.FirstName,c.Email,o.OrderDate,o.TotalAmount as Totalbuy from 
+select c.FirstName,c.Email, FORMAT(o.OrderDate, 'dd MMMM, yyyy') 
+AS OrderDate,o.TotalAmount as Totalbuy from 
 Customers c inner join Orders o on c.CustomerID = o.CustomerID
 inner join Categories ct on o.CategoryID = ct.CategoryID
 where (o.OrderDate < (select GETDATE()-200) and o.TotalAmount > 150)
