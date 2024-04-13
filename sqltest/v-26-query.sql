b372c7d0f2255ce6552afde2652b082506ad3e80
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-04-13 21:22:40 +0600

    v-26 adjusted

--- sqltest/v-26-query.sql
+++ sqltest/v-26-query.sql
@@ -8,6 +8,11 @@ select transaction_date,sum(case when Type = 'Withdrawal' then Amount*-1
 else Amount end) as Total_Amount
 from Transactions group by Transaction_date)
 
-select *,sum(Total_Amount) over (partition by Month(Transaction_date) , Year(transaction_date)
-order by transaction_date
-) from CTE
+SELECT
+    *,
+    SUM(Total_Amount) OVER (
+        PARTITION BY MONTH(Transaction_date), YEAR(Transaction_date)
+        ORDER BY Transaction_date
+    ) AS Running_total
+FROM
+    CTE;
